4c16159a02993a2b1ea5eab23f3fbe502b5af8f0
harshvardhanchand <harshvardhan335@gmail.com>
2023-01-30 18:06:17 +0530

    commit_2

--- ytspamapp/urls.py
+++ ytspamapp/urls.py
@@ -3,5 +3,5 @@ from .import views
 from .views import classify_text
 
 urlpatterns = [
-    path('classify/', classify_text, name='classify_text'),
+    path('', classify_text, name='classify_text'),
 ]
