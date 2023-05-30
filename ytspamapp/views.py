c37c84020755494c0bae513b9d299c5259fa9a8d
Harshvardhan chand <24714562+harshvardhanchand@users.noreply.github.com>
2023-05-30 23:46:50 +0530

    commit_1

--- ytspamapp/views.py
+++ ytspamapp/views.py
@@ -5,34 +5,26 @@ from django.apps import apps
 
 from django.http import JsonResponse
 
-def classify_text(request):  
-    # Get the model and vectorizer from the AppConfig
+def classify_text(request):
     model = apps.get_app_config('ytspamapp').model
     vectorizer = apps.get_app_config('ytspamapp').vectorizer
 
-    # Check if the request method is POST
-    if request.method == 'POST':  
-        # Check if the 'text' field is present in the POST data and it's not empty
+    result = "Please enter a comment to classify."
+
+    if request.method == 'POST':
         if 'text' in request.POST and request.POST['text'].strip():
-            text = request.POST['text']  
-            # Preprocess and transform the input text  
-            X = vectorizer.transform([text])  
-            X = X.toarray()  
-
-            # Use the model to classify the text  
-            classification = model.predict(X)  
-            if classification == 1:  
-                result = "Not Spam"  
-            else:  
-                result = "Spam"  
-            # Return the result to the user
-            return JsonResponse({'classification': result})
-
+            text = request.POST['text']
+            X = vectorizer.transform([text])
+            X = X.toarray()
+            classification = model.predict(X)
+            if classification == 1:
+                result = "Not Spam"
+            else:
+                result = "Spam"
         else:
-            # If the 'text' field is missing or empty, return an error message
-            return JsonResponse({'error': 'The text field is required.'})
+            return render(request, 'ytspamapp/classify_text.html', {'classification': "Error: The text field is required."})
 
-    # If the request method is not POST, render the classify_text page
-    return render(request, 'ytspamapp/classify_text.html')
+    return render(request, 'ytspamapp/classify_text.html', {'classification': result})
+
 
 # Create your views here.
